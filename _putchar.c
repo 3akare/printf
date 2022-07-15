31146c0da0f53e4545efb2cd8431a2292cef25c2
3akare <bakaredavid007@gmail.com>
2022-07-15 03:10:49 +0100

    added _putchar

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - a function that prints out a character
+ * @c: the character to be printed out
+ *
+ * Return 1
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
