6609528e59aa76b4d47ae418f279fc647158cdb1
3akare <bakaredavid007@gmail.com>
2022-07-14 19:41:11 -0700

    updated repo, added _printf.c

--- _printf.c
+++ _printf.c
@@ -1,1 +1,50 @@
 #include "main.h"
+
+/**
+ * _printf - prints out a formatted string
+ * @format: the string to be formatted
+ *
+ * Return: the length of the formatted string
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+
+	va_list list;
+
+	va_start(list, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	while (format[i] != '%')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				count++;
+				i++;
+			}
+			else if (parser(format, i + 1) != NULL)
+			{
+				count += parser(format, i + 1)(list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
+}
--- main.h
+++ main.h
@@ -4,7 +4,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * struct string_format - string format
+ * @character: character pointer argument
+ * @function: function pointer
+ */
+
+typedef struct string_format
+{
+	char *character;
+	int (*function)(va_list);
+} string_format;
 
 int _putchar(char c);
+int _printf(const char *format, ...);
+int (*parser(const char *argument1, int argument2))(va_list);
 
 #endif
