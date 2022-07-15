d116d17c76e6dee94cbc4a61cec39800a799c536
3akare <bakaredavid007@gmail.com>
2022-07-14 20:44:24 -0700

    fixed betty linter bugs

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	va_list list;
-	unsigned int i = 0,count = 0;
+	unsigned int i = 0, count = 0;
 
 	va_start(list, format);
 	if (!format || (format[0] == '%' && format[1] == '\0'))
--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 }
