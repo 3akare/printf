2f8d3b67d8b8ab2ea489af3ff643edc93df87275
bakare <bakaredavid007@gmail.com>
2022-09-22 04:47:04 +0100

    Rev string

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 
 int main(void)
 {
-    int len = 0;
+	int len, len2;
 
-    len = _printf("%r", "\nThis sentence is retrieved from va_args!");
-    len += _printf("%r", "\nThis sentence is retrieved from va_args!");
-    _printf("%d\n", len);
-    return (0);
+	len = _printf("%r", "\nThis sentence is retrieved from va_args!");
+	len2 = printf("!sgra_av morf deveirter si ecnetnes sihT\n");
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
 }
